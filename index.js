8800a96fc127c95004022f45a4f9bae017c34f0d
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-11-10 03:07:11 +0600

    Connection established between nodejs server and mongodb atlas

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,7 +10,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.length("/", (req, res) => {
+// Connecting to MongoDB Atlas
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.h0us0hz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get("/", (req, res) => {
   res.send("Euphonious Whiz server running!");
 });
 
