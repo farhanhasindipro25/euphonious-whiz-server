3bc51ea5fbe404040050b3359e67d4bc2c74255e
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-11-10 18:48:55 +0600

    API created for services, specific selected services, and adding reviews

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -17,11 +17,47 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    // Collection for all services
+    const serviceCollection = client.db("whizDB").collection("services");
+    // Collection for all reviews
+    const reviewCollection = client.db("whizDB").collection("reviews");
+
+    app.get("/home", async (req, res) => {
+      const query = {}; // searching all
+      const cursor = serviceCollection.find(query).limit(3);
+      const limitedServices = await cursor.toArray();
+      res.send(limitedServices);
+    });
+
+    // Making API for all services
+    app.get("/services", async (req, res) => {
+      const query = {}; // searching all
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // Making API for a specific selected service as per id
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // Making API for all reviews
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.error(error));
 
 app.get("/", (req, res) => {
   res.send("Euphonious Whiz server running!");
